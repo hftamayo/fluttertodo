9c98e8d9b12db0f391ba324ccb0296c38c282471
hftamayo <hftamayo@gmail.com>
2023-01-19 15:13:53 -0600

    fixing main method and objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertodo/home_page.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new Home(),
-  ));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const Home(),
+    );
+  }
 }
