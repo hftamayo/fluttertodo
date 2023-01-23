d8f3ba49db3fb973d866f24b428f1cc8e73c3406
hftamayo <hftamayo@gmail.com>
2023-01-23 15:36:20 -0600

    adding a jsonserialize method

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -3,4 +3,13 @@ class Task {
   final String body;
 
   Task({required this.title, required this.body});
+
+  toJSONEncodable() {
+    Map<String, dynamic> mappedTask = new Map();
+
+    mappedTask['title'] = title;
+    mappedTask['body'] = body;
+
+    return mappedTask;
+  }
 }
