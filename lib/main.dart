989a652986ca1b415f0eaf253bf9efa26c48a522
hftamayo <hftamayo@gmail.com>
2023-02-03 14:47:05 -0600

    updating main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,68 @@
+import 'package:fluttertodo/enums/app_theme_enum.dart';
+import 'package:fluttertodo/provider/lang_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
 import 'package:fluttertodo/pages/home_page.dart';
+import 'package:fluttertodo/widgets/mainbar_widget.dart';
+import 'package:provider/provider.dart';
 import 'package:fluttertodo/provider/tasks_provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => TasksProvider()),
+    ChangeNotifierProvider(create: (context) => LangProvider())
+  ], child: const MyApp()));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  ThemeData theme = AppTheme.indigo.theme;
+  static MyAppState? of(BuildContext context) =>
+      context.findAncestorStateOfType<MyAppState>();
+
+  void setTheme(ThemeData theme) {
+    setState(() {
+      this.theme = theme;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: ((context) => TasksProvider()),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Tasks V1',
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate
-        ],
-        supportedLocales: const [Locale('en'), Locale('es')],
-        locale: const Locale('en'),
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-        ),
-        home: const Home(),
+    final TasksProvider tasksProvider = Provider.of<TasksProvider>(context);
+    final LangProvider langProvider = Provider.of<LangProvider>(context);
+    return MaterialApp(
+      theme: theme,
+      debugShowCheckedModeBanner: false,
+      title: 'Tasks V1',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate
+      ],
+      supportedLocales: const [Locale('en'), Locale('es')],
+      locale: Locale(langProvider.languageCode),
+      home: FutureBuilder(
+        future: tasksProvider.loadStore,
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            return const Scaffold(
+              appBar: MainBar(),
+              body: Home(),
+            );
+          } else {
+            return const Scaffold(
+              appBar: MainBar(),
+              body: Center(child: Text('A Loading widget in a future')),
+            );
+          }
+        }),
       ),
     );
   }
