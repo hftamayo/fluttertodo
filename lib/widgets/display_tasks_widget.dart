21d11293bef60c7f3406c4a43d2d77ad6345a2c0
hftamayo <hftamayo@gmail.com>
2023-02-03 14:42:19 -0600

    updating tasks list widget

--- lib/widgets/display_tasks_widget.dart
+++ lib/widgets/display_tasks_widget.dart
@@ -1,40 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../models/task_list.dart';
-import 'package:fluttertodo/provider/tasks_provider.dart';
+import 'package:fluttertodo/models/task.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DisplayTasks extends StatelessWidget {
   const DisplayTasks({
     Key? key,
+    required this.tasks,
   }) : super(key: key);
+
+  final List<Task> tasks;
 
   @override
   Widget build(BuildContext context) {
-    final list = Provider.of<TasksProvider>(context);
-    return ListView.builder(
-      itemBuilder: (_, int index) {
-        return Card(
-          margin: const EdgeInsets.all(4),
-          elevation: 8,
-          child: ListTile(
-            title: Text(
-              list.allTasks[index].title,
-              style: const TextStyle(
-                fontSize: 22,
-                color: Colors.indigo,
-                fontWeight: FontWeight.bold,
+    // TasksProvider tasksProvider =
+    //     Provider.of<TasksProvider>(context, listen: false);
+    if (tasks.isEmpty) {
+      return const _EmptyContext();
+    } else {
+      return ListView.builder(
+        itemBuilder: (_, int index) {
+          return Card(
+            margin: const EdgeInsets.all(4),
+            elevation: 8,
+            child: ListTile(
+              title: Text(
+                // list.items[index].title,
+                tasks[index].title,
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                // list.items[index].body,
+                tasks[index].body,
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
-            subtitle: Text(
-              list.allTasks[index].body,
-              style: const TextStyle(
-                fontSize: 18,
-              ),
-            ),
-          ),
-        );
-      },
-      itemCount: list.allTasks.length,
+          );
+        },
+        // itemCount: list.items.length,
+        itemCount: tasks.length,
+      );
+    }
+  }
+}
+
+class _EmptyContext extends StatelessWidget {
+  const _EmptyContext();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(AppLocalizations.of(context)!.noTaskToDisplay),
     );
   }
 }
