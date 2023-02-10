4fc4405fdfaf72cbd39cc7c42af8e07082f6af4a
hftamayo <hftamayo@gmail.com>
2023-02-10 12:07:26 -0600

    test for displaying task part1

--- test/widgets/display_tasks_test.dart
+++ test/widgets/display_tasks_test.dart
@@ -5,19 +5,23 @@ import 'package:fluttertodo/services/localizations_injected.dart';
 
 void main() {
   group('display tasks list:', () {
-    String title;
-    String body;
-    Task task;
-    testWidgets('render with no tasks', (WidgetTester tester) async {
+    final Task task =
+        Task(title: 'Market Shopping List', body: 'Buy fruits and veggies');
+    late List<Task> tasks;
+    testWidgets('render with no tasks english', (WidgetTester tester) async {
       await tester.pumpWidget(
           const LocalizationsInjected(child: DisplayTasks(tasks: <Task>[])));
       expect(find.byType(DisplayTasks), findsOneWidget);
-    });
-    testWidgets('no tasks display message english',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-          const LocalizationsInjected(child: DisplayTasks(tasks: <Task>[])));
       expect(find.text('No Tasks found'), findsOneWidget);
+    });
+
+    testWidgets('displaying tasks', (WidgetTester tester) async {
+      tasks = task.toMap().entries.map((e) => e.value).toList();
+      await tester
+          .pumpWidget(LocalizationsInjected(child: DisplayTasks(tasks: tasks)));
+      expect(find.byType(DisplayTasks), findsOneWidget);
+      expect(find.text('Market Shopping List'), findsOneWidget);
+      expect(find.text('Buy fruits and veggies'), findsOneWidget);
     });
   });
 }
