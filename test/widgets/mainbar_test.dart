71a6897a11a6b40d4245701341096a219dcf434c
hftamayo <hftamayo@gmail.com>
2023-02-17 11:23:34 -0600

    testing display of three action buttons

--- test/widgets/mainbar_test.dart
+++ test/widgets/mainbar_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fluttertodo/widgets/mainbar_widget.dart';
 import 'package:fluttertodo/services/localizations_injected.dart';
@@ -7,6 +8,15 @@ void main() {
     testWidgets('widget render', (WidgetTester tester) async {
       await tester.pumpWidget(const LocalizationsInjected(child: MainBar()));
       expect(find.byType(MainBar), findsOneWidget);
+    });
+
+    testWidgets('displaying three action buttons', (WidgetTester tester) async {
+      await tester.pumpWidget(const LocalizationsInjected(child: MainBar()));
+      expect(find.widgetWithIcon(MainBar, Icons.brush), findsOneWidget);
+      expect(find.widgetWithIcon(MainBar, Icons.arrow_circle_left),
+          findsOneWidget);
+      expect(find.widgetWithIcon(MainBar, Icons.arrow_circle_right),
+          findsOneWidget);
     });
   });
 }
