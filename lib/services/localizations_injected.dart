7eecabcdae64b77fe1cd7ba2d4e062bf1cb4da46
hftamayo <hftamayo@gmail.com>
2023-02-17 09:18:59 -0600

    adding providers instances

--- lib/services/localizations_injected.dart
+++ lib/services/localizations_injected.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:fluttertodo/provider/tasks_provider.dart';
+import 'package:fluttertodo/provider/lang_provider.dart';
 
 class LocalizationsInjected extends StatelessWidget {
   final Widget child;
@@ -9,15 +12,21 @@ class LocalizationsInjected extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TasksProvider()),
+        ChangeNotifierProvider(create: (context) => LangProvider())
       ],
-      supportedLocales: const [Locale("en", ""), Locale("es", "")],
-      home: child,
+      child: MaterialApp(
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [Locale("en", ""), Locale("es", "")],
+        home: child,
+      ),
     );
   }
 }
