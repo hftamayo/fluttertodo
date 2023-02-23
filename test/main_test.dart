ddbf12b98ae80383d399bcc0b977974574f1c615
hftamayo <hftamayo@gmail.com>
2023-02-23 08:03:41 -0600

    main class unit test part2

--- test/main_test.dart
+++ test/main_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:fluttertodo/main.dart';
 import 'package:fluttertodo/provider/tasks_provider.dart';
+import 'package:fluttertodo/provider/lang_provider.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -9,8 +10,10 @@ void main() {
     "FlutterTodo main class",
     (WidgetTester tester) async {
       await tester.pumpWidget(MultiProvider(providers: [
-        ChangeNotifierProvider(create: (context) => TasksProvider())
+        ChangeNotifierProvider(create: (context) => TasksProvider()),
+        ChangeNotifierProvider(create: (context) => LangProvider())
       ], child: const MyApp()));
+      await tester.pumpAndSettle();
       expect(find.byType(MyApp), findsOneWidget);
     },
   );
